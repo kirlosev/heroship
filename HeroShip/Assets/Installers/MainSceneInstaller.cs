e8ff997167f50da3547cf11fc4b8627cf79dbe3a
Kirill Losev <11328848+kirlosev@users.noreply.github.com>
2023-05-24 21:27:50 +0400

    Create Zenject Installers

---
+++ HeroShip/Assets/Installers/MainSceneInstaller.cs
@@ -0,0 +1,11 @@
+using Zenject;
+
+namespace Installers
+{
+    public class MainSceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
